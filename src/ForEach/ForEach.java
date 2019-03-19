980963b007f0a9d9563941bee827e3b9a8781e9a
Vadim Tynchenko <vadim.tynchenko@icloud.com>
2019-03-19 18:21:26 +0300

    For-Each class init

---
+++ src/ForEach/ForEach.java
@@ -0,0 +1,30 @@
+package ForEach;
+
+public class ForEach {
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+
+        // Использовать разновидность for-each цикла for
+        // для суммирования и отображения значений
+        for (int x : num) {
+            System.out.println("Значение х = " + x);
+            sum += x;
+        }
+
+        System.out.println("Сумма = " + sum + "\n");
+
+        // использование прерывания цикла с помощью brake
+
+        sum = 0;
+        for (int x :
+                num) {
+            System.out.println("Значение х = " + x);
+            sum += x;
+            if (x == 5) break;
+        }
+
+        System.out.println("Сумма = " + sum);
+    }
+
+}
