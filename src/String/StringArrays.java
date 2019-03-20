28a02834380c982ee131432f629f20e9ea11ca1b
Vadim Tynchenko <vadim.tynchenko@icloud.com>
2019-03-21 02:02:13 +0300

    StringArrays init

---
+++ src/String/StringArrays.java
@@ -0,0 +1,24 @@
+package String;
+
+public class StringArrays {
+    public static void main(String[] args) {
+        String str[] = {"Эта ", "строка ", " является ", "тестом."};
+
+        System.out.println("Исходный массив: ");
+        for (String s :
+                str) {
+            System.out.print(s);
+        }
+        System.out.println("\n");
+
+        // изменить строку
+        str[2] = "так же является ";
+        str[3] = "тестом!";
+
+        System.out.println("Измененный массив: ");
+        for (String s :
+                str) {
+            System.out.print(s);
+        }
+    }
+}
