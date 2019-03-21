76b511ef301ca8d657e9f17c205f022c8cf8c8b3
Vadim Tynchenko <vadim.tynchenko@icloud.com>
2019-03-21 03:34:19 +0300

    SubStr init

---
+++ src/String/SubStr.java
@@ -0,0 +1,13 @@
+package String;
+
+public class SubStr {
+    public static void main(String[] args) {
+        String str = "Java - двигатель интернета!";
+
+        // формирование подстроки
+        String subStr = str.substring(7, 26);
+
+        System.out.println("Str = " + str);
+        System.out.println("SubStr = " + subStr);
+    }
+}
