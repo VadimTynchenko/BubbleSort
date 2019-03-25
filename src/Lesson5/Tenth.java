cf8fe2695c1d5c6d2563c7e39b62a8242081a90d
Vadim Tynchenko <vadim.tynchenko@icloud.com>
2019-03-25 21:26:59 +0300

    tenth question init

---
+++ src/Lesson5/Tenth.java
@@ -0,0 +1,11 @@
+package Lesson5;
+
+public class Tenth {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        if (a & b) System.out.println(true); // операция является логической, так
+                                            // как поразрядные операции не совместимы с типом boolean
+    }
+}
