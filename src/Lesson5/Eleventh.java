6c3943a4f8c2bffd600630333cead4380b6a3ecd
Vadim Tynchenko <vadim.tynchenko@icloud.com>
2019-03-25 21:31:39 +0300

    eleventh question init

---
+++ src/Lesson5/Eleventh.java
@@ -0,0 +1,21 @@
+package Lesson5;
+
+public class Eleventh {
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3};
+
+//        for (int i = 0; i < 4; i++) {
+//            System.out.println(num[i]);
+//            System.out.println("Ошибка!");
+//        }
+
+//        for (int i = 0; i > -5; i--) {
+//            System.out.println(num[i]);
+//            System.out.println("Тоже ошибка!");
+//        }
+
+        // возникает ошибка ArrayIndexOutOfBoundsException
+        // так как превышение размера массива и отрицательное значение
+        // массива - неприемлимо
+    }
+}
