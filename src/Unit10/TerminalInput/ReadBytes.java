c722c723b2c17f882b22bb1d27e997f83ebdc77c
Vadim Tynchenko <vadim.tynchenko@icloud.com>
2019-04-17 18:52:12 +0300

    Чтение с клавиатуры может производиться с помощью метода System.in.read(), который записывает символы с клавиатуры в массив байт

---
+++ src/Unit10/TerminalInput/ReadBytes.java
@@ -0,0 +1,16 @@
+package Unit10.TerminalInput;
+
+import java.io.*;
+
+// Чтение байтов с клавиатуры
+public class ReadBytes {
+    public static void main(String[] args) throws IOException {
+        byte[] data = new byte[10];
+        System.out.println("Введите символы.");
+        System.in.read(data);
+        System.out.print("Вы ввели: ");
+        for (int i = 0; i < data.length; i++) {
+            System.out.print((char) data[i]);
+        }
+    }
+}
