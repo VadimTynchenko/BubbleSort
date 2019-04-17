d410293fc8b4696207035a6f321fc1b5e27966f9
Vadim Tynchenko <vadim.tynchenko@icloud.com>
2019-04-17 18:56:52 +0300

    Использование метода System.out.write() для вывода в консоль символа

---
+++ src/Unit10/TerminalInput/WriteDemo.java
@@ -0,0 +1,12 @@
+package Unit10.TerminalInput;
+
+// Демонстрация метода System.out.write()
+public class WriteDemo {
+    public static void main(String[] args) {
+        int b;
+
+        b = 'S';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
